370eee40bbb3fe977b504675b19a949a002ddf12
Priyadarshi Raj <praj-foss@users.noreply.github.com>
2020-07-18 08:39:42 +0530

    Setup SWT dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,20 @@
 plugins {
     java
     application
+    id("com.diffplug.eclipse.mavencentral") version "3.23.0"
 }
 
 repositories {
     mavenCentral()
 }
 
-dependencies {
-}
-
 application {
     mainClassName = "in.praj.demo.SwtApp"
+}
+
+eclipseMavenCentral {
+    release("4.16.0") {
+        implementationNative("org.eclipse.swt")
+        useNativesForRunningPlatform()
+    }
 }
